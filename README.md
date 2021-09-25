0ae8ea97559d20d756e8cf620683c953c4987af9
Varun <vkg0312@gmail.com>
2021-09-25 16:01:01 +0530

    adding workflow

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # oneNeuron_pypi
+oneNeuron_pypi
+
+# Reference -
+[official python docs](https://packaging.python.org/tutorials/packaging-projects/)
+
+[github docs references](https://docs.github.com/en/actions)
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME="oneNeuron_pypi"
+USER_NAME="vkg0312"
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author="VARUN",
+    author_email="vkg0312@gmail.com",
+    description="Its a implementation of perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["numpy","tqdm","logging"]
+)
